bf6980ac3e412df51a43fad7ec02dd3b4fad96ee
OneBrov <pro100brov28@gmail.com>
2024-01-03 17:58:33 +0300

    feat: EnemyAI

--- Assets/TimeTraveler/Scripts/EnemyAI.cs
+++ Assets/TimeTraveler/Scripts/EnemyAI.cs
@@ -1,37 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour, ITurnable
 {
-    [SerializeField] private List<GameAction> _moves = new List<GameAction>();
-    [SerializeField] private bool _isLooped = false;
+    [SerializeField] private GameMap _gameMap;
+    [SerializeField] private List<GameAction> _actions = new List<GameAction>();
+    [SerializeField] private bool _isMovesLooped = true;
 
     private CharacterMover _characterMover;
-    private Vector2Int direction;
-    private int moveStep = 0; 
- 
-    // Start is called before the first frame update
+    private Vector2Int currentDirection;
+    private int moveStep = 0;
+
+    // Start is called before the first fr ame update
     void Start()
     {
         _characterMover = GetComponent<CharacterMover>();
+        _characterMover.Init(_gameMap);
     }
 
     public void OnTurn()
     {
-
-        if (moveStep < _moves.Count)
+        if (moveStep < _actions.Count)
         {
-            var currentMove = _moves[moveStep++];
+            var currentAction = _actions[moveStep++];
+
+            bool isMove = _characterMover.TryCastDirection(currentAction, out var direction);
+            if (isMove)
+            {
+                Debug.Log($"Moving into{direction}");
+                _characterMover.Move(direction);
+                currentDirection = direction;
+            }
         }
 
-        if (moveStep >= _moves.Count && _isLooped)
+        if (moveStep >= _actions.Count && _isMovesLooped)
         {
             moveStep = 0;
         }
 
-
         Debug.Log("My turn!");
+    }
+
+    float counter = 0;
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (counter >= 1)
+        {
+            OnTurn();
+            counter = 0;
+        }
     }
 }
