f556faccd5e12d49cfcd7de45f895cf6a72833fe
OneBrov <pro100brov28@gmail.com>
2024-01-04 20:40:18 +0300

    chore: scenes

--- Assets/TimeTraveler/Scripts/Interaction/Door.cs
+++ Assets/TimeTraveler/Scripts/Interaction/Door.cs
@@ -20,6 +20,7 @@ public class Door : Interactive
         if ( _isOpened )
         {
             Debug.Log("You Win!");
+            GameManager.Instance.Win();
         }
         else
         {
