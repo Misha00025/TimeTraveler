0de55b2d78bf858f3e8c8d19f7443e83b74f6ca9
Misha00025 <misha00025@mail.ru>
2024-01-10 07:39:07 +0300

    feat: task sequencer

---
+++ Assets/TimeTraveler/Scripts/Model/TasksManagment/TaskSequencer.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public abstract class Task
+    {
+        public enum Priority
+        {
+            Low,
+            Medium,
+            High,
+            Max
+        }
+
+        private readonly Object _owner;
+
+        public Task(Object owner)
+        {
+            _owner = owner;
+        }
+
+        public Object Owner { get { return _owner; } }
+
+        public abstract void Run();
+        public abstract bool IsRunning();
+    }
+
+    public interface ITaskSequencer
+    {
+        int GetTasksCount(Task.Priority priority);
+        void AddTask(Task task, Task.Priority priority = Task.Priority.Medium);
+        void RunTasks(Task.Priority priority);
+    }
+
+    public class TaskSequencer : ITaskSequencer
+    {
+        private readonly Dictionary<Task.Priority, List<Task>> _tasks;
+
+        public TaskSequencer()
+        {
+            _tasks = new Dictionary<Task.Priority, List<Task>>();
+            foreach (Task.Priority priority in Enum.GetValues(typeof(Task.Priority)))
+            {
+                _tasks.Add(priority, new List<Task>());
+            }
+        }
+
+        public int GetTasksCount(Task.Priority priority)
+        {
+            return _tasks[priority].Count;
+        }
+
+        public void AddTask(Task task, Task.Priority priority = Task.Priority.Medium)
+        {
+            _tasks[priority].Add(task);
+        }
+
+        public void RunTasks(Task.Priority priority)
+        {
+            Task[] tasks = _tasks[priority].ToArray();
+            _tasks[priority].Clear();
+            foreach (Task task in tasks)
+            {
+                task.Run();
+            }
+        }
+    }
+}
